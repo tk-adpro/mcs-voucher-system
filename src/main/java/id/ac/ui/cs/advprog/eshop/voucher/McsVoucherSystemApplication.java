9e517bb905de399f05235c6fce9241b5adbc4c17
Anisa Maharani <anismyes@gmail.com>
2024-04-06 15:54:54 +0800

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/voucher/McsVoucherSystemApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.voucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsVoucherSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsVoucherSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/voucher/McsVoucherSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsVoucherSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
