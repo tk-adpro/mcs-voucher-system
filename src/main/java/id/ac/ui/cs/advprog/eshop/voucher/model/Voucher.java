2996960f381a8ee2590b2c43c42025375aa73748
azmyar <azmy.arya.rizaldi@gmail.com>
2024-04-27 09:51:36 +0700

    [GREEN] implement Voucher model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/voucher/model/Voucher.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.advprog.eshop.voucher.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+// TODO add discount amount
+@Getter @Setter
+public class Voucher {
+    private String voucherId;
+    private String voucherName;
+    private String voucherDescription;
+    private LocalDate voucherStartDate;
+    private LocalDate voucherEndDate;
+    private double voucherDiscount;
+    private int voucherUsageLimit;
+}
