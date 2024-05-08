3b58c2013801f68c3d2e5f65b2ad157f38e6bda0
azmyar <azmy.arya.rizaldi@gmail.com>
2024-05-08 21:41:12 +0700

    fix: database authorization

--- src/main/java/id/ac/ui/cs/advprog/eshop/voucher/McsVoucherSystemApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/voucher/McsVoucherSystemApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class McsVoucherSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(McsVoucherSystemApplication.class, args);
