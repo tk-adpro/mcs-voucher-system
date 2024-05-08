54a3fba0787672c0ba45f477743fedc5b919e6a4
azmyar <azmy.arya.rizaldi@gmail.com>
2024-05-08 20:53:51 +0700

    [GREEN-REFACTOR] refactor VoucherController (JPA)

--- src/main/java/id/ac/ui/cs/advprog/eshop/voucher/controller/VoucherController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/voucher/controller/VoucherController.java
@@ -14,32 +14,38 @@ import java.util.List;
 public class VoucherController {
 
     @Autowired
-    private VoucherService<Voucher> voucherService;
+    private VoucherService voucherService;
 
     @GetMapping("/list")
-    public ResponseEntity<List <Voucher>> voucherList() {
-        return ResponseEntity.ok(voucherService.findAllVoucher());
+    public ResponseEntity<List<Voucher>> getAllVouchers() {
+        List<Voucher> vouchers = voucherService.findAll();
+        return ResponseEntity.ok(vouchers);
+    }
+
+    @GetMapping("/get/{voucherId}")
+    public ResponseEntity<Voucher> getVoucherById(@PathVariable String voucherId) {
+        return voucherService.findById(voucherId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/create")
     public ResponseEntity<Voucher> createVoucherPost (@RequestBody Voucher voucher) {
-        Voucher createdVoucher = voucherService.createVoucher(voucher);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdVoucher);
+        Voucher createdProduct = voucherService.create(voucher);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
     }
 
     @PutMapping("/edit/{voucherId}")
     public ResponseEntity<Voucher> editVoucherPost(@PathVariable String voucherId, @RequestBody Voucher voucher) {
-        Voucher currentVoucher = voucherService.getVoucherById(voucherId);
-        return currentVoucher != null ?
-                ResponseEntity.ok(voucherService.editVoucher(voucher)) :
-                ResponseEntity.notFound().build();
+        return voucherService.findById(voucherId)
+                .map(p -> ResponseEntity.ok(voucherService.update(voucher)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/delete/{voucherId}")
     public ResponseEntity<Voucher> deleteVoucherPost(@PathVariable String voucherId) {
-        Voucher deletedVoucher = voucherService.deleteVoucher(voucherId);
-        return deletedVoucher != null ?
-                ResponseEntity.ok(deletedVoucher) :
+        return voucherService.delete(voucherId) ?
+                ResponseEntity.noContent().build() :
                 ResponseEntity.notFound().build();
     }
 }
