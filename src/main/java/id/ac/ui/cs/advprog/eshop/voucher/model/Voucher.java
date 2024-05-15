ecaf2deace7478749c2897ca4325ad698083029f
azmyar <azmy.arya.rizaldi@gmail.com>
2024-05-15 18:32:40 +0700

    [GREEN-REFACTOR] implement builder design pattern in Voucher model

--- src/main/java/id/ac/ui/cs/advprog/eshop/voucher/model/Voucher.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/voucher/model/Voucher.java
@@ -9,6 +9,8 @@ import java.time.LocalDate;
 @Table(name = "voucher")
 @Getter @Setter
 public class Voucher {
+
+    // Required parameters
     @Id
     @Column(name = "voucher_id", updatable = false, nullable = false)
     private String voucherId;
@@ -19,15 +21,75 @@ public class Voucher {
     @Column(name = "voucher_description")
     private String voucherDescription;
 
+    @Column(name = "voucher_discount")
+    private double voucherDiscount;
+
+    // Optional parameters
+    @Column(name = "voucher_type")
+    private String voucherType;
+
     @Column(name = "voucher_start_date")
     private LocalDate voucherStartDate;
 
     @Column(name = "voucher_end_date")
     private LocalDate voucherEndDate;
 
-    @Column(name = "voucher_discount")
-    private double voucherDiscount;
-
     @Column(name = "voucher_usage_limit")
     private int voucherUsageLimit;
+
+    public Voucher() {
+    }
+    private Voucher(VoucherBuilder builder) {
+        this.voucherId = builder.voucherId;
+        this.voucherName = builder.voucherName;
+        this.voucherDescription = builder.voucherDescription;
+        this.voucherDiscount = builder.voucherDiscount;
+
+        this.voucherType = builder.voucherType;
+        this.voucherStartDate = builder.voucherStartDate;
+        this.voucherEndDate = builder.voucherEndDate;
+        this.voucherUsageLimit = builder.voucherUsageLimit;
+    }
+
+    public static class VoucherBuilder {
+
+        // Required parameters
+        private final String voucherId;
+        private final String voucherName;
+        private final String voucherDescription;
+        private final double voucherDiscount;
+
+        // Optional parameters
+        private String voucherType;
+        private LocalDate voucherStartDate;
+        private LocalDate voucherEndDate;
+        private int voucherUsageLimit;
+
+        public VoucherBuilder(String voucherId, String voucherName, String voucherDescription, double voucherDiscount) {
+            this.voucherId = voucherId;
+            this.voucherName = voucherName;
+            this.voucherDescription = voucherDescription;
+            this.voucherDiscount = voucherDiscount;
+        }
+
+        public VoucherBuilder setType(String type) {
+            this.voucherType = type;
+            return this;
+        }
+
+        public VoucherBuilder setVoucherDate(LocalDate voucherStartDate, LocalDate voucherEndDate) {
+            this.voucherStartDate = voucherStartDate;
+            this.voucherEndDate = voucherEndDate;
+            return this;
+        }
+
+        public VoucherBuilder setUsageLimit (int voucherUsageLimit) {
+            this.voucherUsageLimit = voucherUsageLimit;
+            return this;
+        }
+
+        public Voucher build() {
+            return new Voucher(this);
+        }
+    }
 }
